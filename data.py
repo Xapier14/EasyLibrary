0fa51e3dee7152a2df143a6713e59d84f94384d1
Lance Daniel Crisang <lancecrisang14@gmail.com>
2022-04-27 14:08:35 +0800

    refactor: add return type hint to data.GetDataStore()

--- data.py
+++ data.py
@@ -2,12 +2,13 @@
 #   Handles DataStore Singleton Functionality
 
 from argparse import ArgumentError
+import lib.datastoreInterface as dsi
 import lib.localDatastore as localDatastore
 
 __datastoreInstance = None
 __datastoreDatabase = "local"
 
-def GetDataStore(database = "local"):
+def GetDataStore(database = "local") -> dsi.DataStoreInterface:
     global __datastoreInstance
     global __datastoreDatabase
 
--- lib/datastoreInterface.py
+++ lib/datastoreInterface.py
@@ -2,6 +2,8 @@
 #   Defines needed functionality for DataStore implementations
 
 class DataStoreInterface:
+    def ReloadData(self):
+        raise NotImplementedError("Method not yet implemented.")
     def GetUser(self, username):
         raise NotImplementedError("Method not yet implemented.")
     def GetBook(self, isbn):
