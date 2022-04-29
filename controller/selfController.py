78e064bf3756d4af26d645cecab502520de52b51
Lance Daniel Crisang <lancecrisang14@gmail.com>
2022-04-29 15:38:21 +0800

    feat: add functionality to search button

--- controller/selfController.py
+++ controller/selfController.py
@@ -1,6 +1,7 @@
 # Controller for Self-Service mode
 import PySimpleGUI as sg
 import app
+import make
 from controller.baseController import Controller
 
 class SelfController(Controller):
@@ -12,6 +13,21 @@ class SelfController(Controller):
             case sg.WIN_X_EVENT:
                 if (self.WinClose()):
                     return True
+            case "-button-search-":
+                if (self.button_search(model)):
+                    return True
+            case "-button-borrow-":
+                if (self.button_borrow(model)):
+                    return True
+            case "-button-return-":
+                if (self.button_return(model)):
+                    return True
+            case "-button-due-":
+                if (self.button_due(model)):
+                    return True
+            case "-button-history-":
+                if (self.button_history(model)):
+                    return True
             case "-button-logout-":
                 if (self.button_logout(model)):
                     return True
@@ -22,6 +38,26 @@ class SelfController(Controller):
     
     def Update(self, model):
         return
+
+    def button_search(self, model):
+        app.PushPairToStack(make.MakeSearchService())
+        return True
+
+    def button_borrow(self, model):
+        
+        return True
+
+    def button_return(self, model):
+        
+        return True
+
+    def button_due(self, model):
+        
+        return True
+
+    def button_history(self, model):
+        
+        return True
 
     def button_logout(self, model):
         app.PopControllerFromStack()
