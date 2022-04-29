a9bfa657e29f8d68d282f28e5aabac39b9937602
Lance Daniel Crisang <lancecrisang14@gmail.com>
2022-04-29 16:36:31 +0800

    feat: add user save and load

--- datastore/localDatastore.py
+++ datastore/localDatastore.py
@@ -1,13 +1,24 @@
 # LocalDataStore
 #   DataStore that functions on a local JSON database.
 
-from re import S
+import json
+import base64
+from datamodel.user import User
 from datastore.datastoreInterface import DataStoreInterface
+from enums import UserEnum
 
 class LocalDataStore(DataStoreInterface):
     def __init__(self):
         self.ReloadData()
         return
+
+    def Prettify(jsonFile):
+        f = open(jsonFile, "r")
+        userJson = json.load(f)
+        f.close()
+        f = open(jsonFile, "w")
+        json.dump(userJson, f, indent=4)
+        f.close()
 
     def ReloadData(self):
         # Users
@@ -21,6 +32,40 @@ class LocalDataStore(DataStoreInterface):
         # Transactions (Returns)
         self.Returned = []
 
+        f = open("localDb/users.json", "r")
+
+        users = json.load(f)
+        for userEntry in users:
+            userType = userEntry["isAdmin"] if UserEnum.Admin else UserEnum.User
+            user = User(userEntry["username"], userType, base64.b64decode(bytes(userEntry["password"], "utf-8")), base64.b64decode(bytes(userEntry["salt"], "utf-8")), userEntry["fullName"])
+            self.Users.append(user)
+
+        f.close()
+        return
+    
+    def SaveData(self):
+        # Write user data
+        f = open("localDb/users.json", "w")
+        f.write("[")
+        for user in self.Users:
+            f.write("{")
+
+            f.write("\"username\": \"" + user.GetUsername() + "\",")
+            f.write("\"password\": \"" + base64.b64encode(user.GetHashedPassword()).decode("utf-8") + "\",")
+            f.write("\"salt\": \"" + base64.b64encode(user.GetSalt()).decode("utf-8") + "\",")
+            f.write("\"fullName\": \"" + user.GetFullName() + "\",")
+            if user.GetAccessLevel() == UserEnum.Admin:
+                f.write("\"isAdmin\": true")
+            else:
+                f.write("\"isAdmin\": false")
+
+            if user == self.Users[-1]:
+                f.write("}")
+            else:
+                f.write("},")
+        f.write("]")
+        f.close()
+        LocalDataStore.Prettify("localDb/users.json")
         
         return
     
