ab5197892a11b56c8382f1adfa83470c1bd3b837
Lance Daniel Crisang <lancecrisang14@gmail.com>
2022-05-01 08:58:48 +0800

    feat: add search function

--- controller/searchController.py
+++ controller/searchController.py
@@ -1,6 +1,7 @@
 # Controller for Self-Service search function
 import PySimpleGUI as sg
 import app
+import data
 from controller.baseController import Controller
 
 class SearchController(Controller):
@@ -15,6 +16,9 @@ class SearchController(Controller):
             case "-button-back-":
                 if (self.button_back(model)):
                     return True
+            case "-input-query-":
+                if (self.input_query(model, values["-input-query-"])):
+                    return True
         return False
 
     def WinClose(self):
@@ -26,3 +30,18 @@ class SearchController(Controller):
     def button_back(self, model):
         app.PopControllerFromStack()
         return True
+
+    def input_query(self, model, inputText):
+        print("Text Updated!")
+        model.query = inputText
+        model.books = data.GetDataStore().GetAllBooks()
+        if (model.query != ""):
+            filteredBooks = []
+            for book in model.books:
+                if (book.GetTitle().lower().find(model.query.lower())!= -1 or book.GetAuthor().lower().find(model.query.lower())!= -1 or book.GetISBN().lower().find(model.query.lower())!= -1):
+                    print("Found match!")
+                    filteredBooks.append(book)
+            model.books = filteredBooks
+            print("Books: " + str(len(model.books)))
+        self.ModelUpdated(model)
+        return False
--- view/searchView.py
+++ view/searchView.py
@@ -19,12 +19,21 @@ class SearchView(ViewInterface):
         else:
             bookList.append(["No books found."])
 
-        listColumn = [[sg.Table(bookList, headings=["Title", "Author", "ISBN", "Year", "Genre", "Publisher"], expand_x=True, expand_y=True, enable_click_events=True, vertical_scroll_only=True, justification="left") ]]
+        listColumn = [[sg.Table(bookList, headings=["Title", "Author", "ISBN", "Year", "Genre", "Publisher"], expand_x=True, expand_y=True, enable_click_events=True, vertical_scroll_only=True, justification="left", key="-table-books-") ]]
         detailsColumn = [[sg.Text("details")]]
         layout = [  [sg.Text("EasyLibrary - Self-Service Mode")],
                     [sg.Text("Search for books")],
                     [sg.Column(listColumn, expand_y=True, expand_x=True), sg.Column(detailsColumn, expand_y=True, expand_x=True)],
-                    [sg.Push(), sg.Button("Go Back", key="-button-back-", size=buttonSize)] ]
+                    [sg.Text("Search Query: "), sg.Input("", size=(50, 1), key="-input-query-", enable_events=True), sg.Push(), sg.Button("Go Back", key="-button-back-", size=buttonSize)] ]
         return layout
     def Update(self, window, model):
+        bookList = []
+        if (len(model.books) > 0):
+            for book in model.books:
+                bookEntry = [book.GetTitle(), book.GetAuthor(), book.GetISBN(), book.GetYear(), book.GetGenre(), book.GetPublisher()]
+                bookList.append(bookEntry)
+        else:
+            bookList.append(["No books found."])
+            
+        window["-table-books-"].update(values=bookList)
         return
